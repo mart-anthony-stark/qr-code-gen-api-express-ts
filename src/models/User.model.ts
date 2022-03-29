6629ecafef1b4df206896007627767d64e64e4e2
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 23:17:04 +0800

    fixed minlength

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 8 },
+    password: { type: String, required: true, minLength: 8 },
     role: { type: String, default: "user", enum: ["user", "admin"] },
   },
   { timestamps: true }
