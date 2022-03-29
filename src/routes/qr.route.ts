4176153bc25f02c00073799931094c270c1bcfd5
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 22:14:35 +0800

    add middlewares

--- src/routes/qr.route.ts
+++ src/routes/qr.route.ts
@@ -1,10 +1,16 @@
 import QRController from "../controller/qr.controller";
 import { Router } from "express";
+import { verifyCurrentUser, verifyToken } from "../utils/token";
 const router = Router();
 
-router.get("/", QRController.getAllQR);
-router.get("/:id", QRController.getOneQR);
-router.post("/", QRController.createQR);
+router.get("/all/:uid", verifyToken, verifyCurrentUser, QRController.getAllQR);
+router.get(
+  "/:id/user/:uid",
+  verifyToken,
+  verifyCurrentUser,
+  QRController.getOneQR
+);
+router.post("/", verifyToken, QRController.createQR);
 router.put("/:id", QRController.updateQR);
 router.delete("/:id", QRController.deleteQR);
 
