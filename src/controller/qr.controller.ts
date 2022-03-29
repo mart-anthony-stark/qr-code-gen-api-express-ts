686b1086f9e3c5859894fe9116922702844840ea
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 22:20:47 +0800

    add getAllQRByUser

--- src/controller/qr.controller.ts
+++ src/controller/qr.controller.ts
@@ -9,13 +9,18 @@ export default {
     res.send(qr);
   },
 
-  getOneQR: async (req: Request, res: Response) => {
-    const qr = await QR.findOne(req.params);
+  getAllQRByUser: async (req: Request, res: Response) => {
+    const qr = await QR.find({});
+    res.send(qr);
+  },
+
+  getOneQR: async (req: IGetUserAuthInfoRequest, res: Response) => {
+    const qr = await QR.findOne({ _id: req.params.id, user: req.user._id });
     res.send(qr);
   },
 
   createQR: async (req: IGetUserAuthInfoRequest, res: Response) => {
-    const qr = new QR({user: req.user._id, ...req.body});
+    const qr = new QR({ user: req.user._id, ...req.body });
     await qr.save();
     res.send(qr);
   },
