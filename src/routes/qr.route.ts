65864af2a2a6ebf7d04199e454b0b7dd3eb51c26
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 22:27:47 +0800

    add aggregators

--- src/routes/qr.route.ts
+++ src/routes/qr.route.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { verifyAdmin, verifyCurrentUser, verifyToken } from "../utils/token";
 const router = Router();
 
-router.get("/all/", verifyToken, verifyAdmin, QRController.getAllQR);
+router.get("/all", verifyToken, verifyAdmin, QRController.getAllQR);
 
 router.get(
   "/all/:uid",
@@ -16,8 +16,18 @@ router.get("/:id", verifyToken, QRController.getOneQR);
 
 router.post("/", verifyToken, QRController.createQR);
 
-router.put("/:id", QRController.updateQR);
+router.put(
+  "/:id/user/:uid",
+  verifyToken,
+  verifyCurrentUser,
+  QRController.updateQR
+);
 
-router.delete("/:id", QRController.deleteQR);
+router.delete(
+  "/:id/user/:uid",
+  verifyToken,
+  verifyCurrentUser,
+  QRController.deleteQR
+);
 
 export default router;
