0605fbcc495a826a49ee5dc4d4d150807f7c00a6
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-31 19:32:40 +0800

    add checkAuth route

--- dist/routes/auth.route.js
+++ dist/routes/auth.route.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const auth_controller_1 = __importDefault(require("../controller/auth.controller"));
+const token_1 = require("../utils/token");
 const router = (0, express_1.Router)();
+router.post("/islogged", token_1.verifyToken, auth_controller_1.default.checkAuth);
 router.post("/register", auth_controller_1.default.signup);
 router.post("/login", auth_controller_1.default.login);
 exports.default = router;
