8f77491a570595a34c6e5d359293f3c8cb6bd68e
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-31 19:35:24 +0800

    change islogged http method to get

--- dist/routes/auth.route.js
+++ dist/routes/auth.route.js
@@ -7,7 +7,7 @@ const express_1 = require("express");
 const auth_controller_1 = __importDefault(require("../controller/auth.controller"));
 const token_1 = require("../utils/token");
 const router = (0, express_1.Router)();
-router.post("/islogged", token_1.verifyToken, auth_controller_1.default.checkAuth);
+router.get("/islogged", token_1.verifyToken, auth_controller_1.default.checkAuth);
 router.post("/register", auth_controller_1.default.signup);
 router.post("/login", auth_controller_1.default.login);
 exports.default = router;
