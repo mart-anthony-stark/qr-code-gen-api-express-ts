6aa16bdf87d9ffea2ff44702b43c30666b93931f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-31 10:10:19 +0800

    add dist

---
+++ dist/models/QR.model.js
@@ -0,0 +1,18 @@
+"use strict";
+const mongoose = require("mongoose");
+const QRSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User is required"],
+    },
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+    },
+    value: {
+        type: String,
+        required: [true, "Value is required"],
+    },
+});
+module.exports = mongoose.model("QRcode", QRSchema);
