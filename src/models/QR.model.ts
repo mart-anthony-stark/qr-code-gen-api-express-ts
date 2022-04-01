52491ae477f3e26842c2c805f5875e2a210b4a8d
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-01 19:59:44 +0800

    add time stamps to qr model

--- src/models/QR.model.ts
+++ src/models/QR.model.ts
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const QRSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "User is required"],
+const QRSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    value: {
+      type: String,
+      required: [true, "Value is required"],
+    },
   },
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-  },
-  value: {
-    type: String,
-    required: [true, "Value is required"],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("QRcode", QRSchema);
