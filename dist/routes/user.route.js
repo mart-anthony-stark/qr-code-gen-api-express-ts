c4546db3b43bf3cbb3ab02999c4cd32576c56d32
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-02 10:57:12 +0800

    add user router

---
+++ dist/routes/user.route.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const controller = require("../controller/");
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+module.exports = router;
+exports.default = router;
