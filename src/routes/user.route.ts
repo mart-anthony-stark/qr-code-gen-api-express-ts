b91c222a5d18f5e7cb4c0b5cd23569cc9eeb7343
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-02 10:59:41 +0800

    add user subscription route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,10 @@
-const controller = require("../controller/");
+const controller = require("../controller/user.controller");
 import { Router } from "express";
+import { verifyToken } from "../utils/token";
 
 const router = Router();
+
+router.put("/subscription", verifyToken, controller.upgradeSubscription);
 
 module.exports = router;
 export default router;
