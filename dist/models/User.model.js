cce3a3f0c9713b7867943af87a2dd0fcd3dc13b1
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-02 10:56:54 +0800

    add user model

--- dist/models/User.model.js
+++ dist/models/User.model.js
@@ -6,5 +6,6 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minLength: 8 },
     role: { type: String, default: "user", enum: ["user", "admin"] },
+    subscription: { type: String, default: "free", enum: ["free", "premium"] },
 }, { timestamps: true });
 exports.default = mongoose.model("User", userSchema);
