ac61329afd4042d707ebb23a497c1bdb57f1fa55
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-02 19:04:22 +0800

    add Guard for free account subscription limit

--- src/controller/qr.controller.ts
+++ src/controller/qr.controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import { IGetUserAuthInfoRequest } from "../types/Request.types";
 
 const QR = require("../models/QR.model");
+const User = require("../models/User.model");
+
+// QR Code number limit for free accounts
+const qrLimit = 5;
 
 export default {
   getAllQR: async (req: Request, res: Response) => {
@@ -20,9 +24,22 @@ export default {
   },
 
   createQR: async (req: IGetUserAuthInfoRequest, res: Response) => {
+    // Guard for free account subscription limit
+    if (req.user.subscription === "free") {
+      const codes = await QR.find({ user: req.user._id });
+
+      if (codes.length === qrLimit)
+        return res
+          .status(403)
+          .send({
+            success: false,
+            msg: "You've reached the limit for free account",
+          });
+    }
+
     const qr = new QR({ user: req.user._id, ...req.body });
     await qr.save();
-    res.send(qr);
+    res.send({ success: true, qr });
   },
 
   updateQR: async (req: Request, res: Response) => {
