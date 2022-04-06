027df87c1e6c1cfc45cc8843c0dac20035387174
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-06 19:06:20 +0800

    fixed update subscription

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from "express";
+import UserModel from "../models/User.model";
 import { IGetUserAuthInfoRequest } from "../types/Request.types";
-const User = require("../models/User.model");
+// const User = require("../models/User.model");
 
 module.exports = {
-  upgradeSubscription: async (req: IGetUserAuthInfoRequest, res: Response) => {
-    await User.findByIdAndUpdate(req.user.id, {
-      $set: { subscription: req.body.subscription },
-    });
-    res.send({success:true})
+  upgradeSubscription: async (req: any, res: Response) => {
+    try {
+      const user = await UserModel.findOneAndUpdate(
+        { _id: req.user.id },
+        {
+          $set: { subscription: req.body.subscription },
+        },
+        { new: true }
+      );
+      // delete user.password;
+      res.send({ success: true, user });
+    } catch (e) {
+      console.log(e);
+      res.status(401).send({ success: false, msg: e });
+    }
   },
 };
